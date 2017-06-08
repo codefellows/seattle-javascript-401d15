5e151cbeae57b11911621ddaede50c2a9284a488
Scott Schmidt <sjschmidt44@gmail.com>
2017-06-08 16:40:37 -0700

    update lecture code for day 32

--- class-32-testing-components/demo/app/component/gallery/gallery-item/gallery-item.js
+++ class-32-testing-components/demo/app/component/gallery/gallery-item/gallery-item.js
@@ -7,10 +7,12 @@ module.exports = {
     this.$onInit = () => {
       $log.debug('Gallery Item Controller')
 
+
       this.showEditGallery = false
 
       this.makeCurrentGallery = () => {
         $log.log('ran the thing!')
+        console.log('gall id', this.gallery._id)
         $rootScope.$emit('updateCurrentGallery', this.gallery._id)
       }
     }
--- class-32-testing-components/demo/app/view/home/home-controller.js
+++ class-32-testing-components/demo/app/view/home/home-controller.js
@@ -32,14 +32,13 @@ module.exports = [
 
       $rootScope.$on('locationChangeSuccess', this.fetchGalleries)
       $rootScope.$on('newGalleryCreated', this.fetchGalleries)
-      $rootScope.$on('updateCurrentGallery', (galleryId) => {
-        console.log('gallery arg', galleryId);
+      $rootScope.$on('updateCurrentGallery', (eve, galleryId) => {
+        // NOTE: There's an event object (eve parameter) passed in, so have to account for that before the arguments
         for(let i = 0; i < this.galleries.length; i++) {
           if(this.galleries[i]._id === galleryId) {
             this.currentGallery = this.galleries[i]
             break
           }
-          console.log('scott was here', this.currentGallery);
         }
       })
       this.fetchGalleries()
